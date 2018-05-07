54542d7f4f0b7edfa922871caf7a18cf9b1b418a
Amber Sargeant <asargean@ucsc.edu>
2018-05-06 19:08:38 -0700

    Added initial game files

---
+++ js/main.js
@@ -0,0 +1,92 @@
+var game;
+window.onload =  function() {
+//Creates instance of game object and changes canvas size
+game = new Phaser.Game(800, 600, Phaser.CANVAS);
+game.state.add('MainMenu', MainMenu);
+game.state.add('GamePlay', GamePlay);
+game.state.add('GameOver', GameOver);
+game.state.start('MainMenu');
+}
+//Declaring variables.
+var MainMenu = function(game) {};
+var player;
+
+//Decalares Mainmenu prototype
+MainMenu.prototype = {
+	//loads the mainmenu images
+	preload: function(){
+		console.log('MainMenu: preload');
+	},
+	//creates all of the assets
+	create: function(){
+		console.log("MainMenu: create'")
+
+	},
+	update: function() {
+		//console.log("MainMenu: update");
+		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+			game.state.start('GamePlay')
+		}
+	}
+}
+
+//Defines actual gameplay
+var GamePlay = function(game){};
+//loads gameplay assets
+GamePlay.prototype = {
+	preload: function(){
+		console.log("Gameplay: preload");
+		game.load.image('greyBackground', 'assets/img/greyBackground.png');
+		game.load.image('greyPlatform', 'assets/img/greyPlatform.png');
+		game.load.atlas('player', 'assets/img/player.png', 'assets/img/player.json');
+	},
+	//creates the assets
+	create: function(){
+		console.log("Gameplay: Create");
+		 //enables physics 
+		 game.physics.startSystem(Phaser.Physics.ARCADE);
+		//adds color background(placeholder)
+		 var greyBackground = game.add.sprite(0, 0, 'greyBackground');
+		 //adds platforms to group
+		 var platforms = game.add.group();
+		 //  enables body for that group
+         platforms.enableBody = true;
+    	 // creates the ground
+    	 var greyPlatform = platforms.create(0, 510, 'greyPlatform');
+    	 //  scales the ground so it can fit the game width
+   		 // The ground doesnt fall or move when jumped on
+   		 greyPlatform.body.immovable = true;
+   		 //adds player
+   		 this.player = new Player(game,'player');
+   		 this.game.add.existing(this.player);
+	},
+	update: function (){
+		//console.log("Gameplay: update");
+	 	if(game.input.keyboard.isDown(Phaser.Keyboard.Q)){
+			game.state.start('GameOver');
+		}
+    
+	}
+}
+
+//Defines gameover function
+var GameOver = function(game){};
+GameOver.prototype = {
+	//preloads assets
+	preload: function(){
+		console.log("Gameover:Preload");
+		
+	},
+	//creates assets
+	create: function(){
+		console.log("Gameover: create");
+	},
+	update: function(){
+		//console.log("Gameover: Update");
+		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+			game.state.start('MainMenu');
+		}
+
+	}
+}
+
---
+++ js/player.js
@@ -0,0 +1,35 @@
+function Player(game, frame){
+	//call to Phaser.Sprite // new Sprite(game, x, y, key, frame)
+	Phaser.Sprite.call(this, game, 64,game.world.height-80,frame);
+
+	// add properties
+	this.anchor.set(0.5);
+	game.physics.enable(this);
+	this.enableBody = true;
+	this.body.collideWorldBounds = true;
+	this.body.bounce.y = 0.2;
+    this.body.gravity.y = 800;
+    this.animations.add('walk',[0,1,2,3,4,5],10, true);
+	this.animations.add('idle', ['walk1'], 30, false);
+	this.body.velocity.x = 0;
+}
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function(){
+	this.body.velocity.x = 0;
+	//if right key is pressed, player runs to the right
+	if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+		this.body.velocity.x = 100;
+		this.scale.x = 1;
+		this.animations.play('walk');	
+		//else the idle animation plays
+		}else if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+		this.body.velocity.x = -100;
+		this.scale.x = -1;
+		this.animations.play('walk');	
+		}else{
+			this.animations.play('idle');
+	}	
+}
